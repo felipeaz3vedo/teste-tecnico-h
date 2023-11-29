009cb25b2297a2f5879982df636c8798c1f593c4
Felipe Pessoa <felipedeazevedopessoa@gmail.com>
2023-11-29 01:48:31 -0300

    feat(backend): create user model on prisma schema

---
+++ backend/prisma/migrations/20231129014256_create_user_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "deleted_at" TIMESTAMP(0),
+    "created_at" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(0) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
