2e2588565e01a22a9a048b9a5bff5fb4cf7dabf9
Felipe Pessoa <felipedeazevedopessoa@gmail.com>
2023-12-01 00:00:28 -0300

    refactor(backend): enhance error handling and auth validation

--- backend/src/error/http-exception.filter.ts
+++ backend/src/error/http-exception.filter.ts
@@ -4,20 +4,17 @@ import {
   ArgumentsHost,
   HttpException
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
 
     response.status(status).json({
       statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
       message: exception.message
     });
   }
--- backend/src/modules/auth/schemas/register.schema.ts
+++ backend/src/modules/auth/schemas/register.schema.ts
@@ -1,9 +1,11 @@
 import z from 'zod';
 
-export const registerSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string()
-});
+export const registerSchema = z
+  .object({
+    name: z.string().min(4, 'Name must to be at least 4 characters'),
+    email: z.string().email(),
+    password: z.string().min(6, 'Passoword must to be at least 6 characters')
+  })
+  .required();
 
 export type RegisterSchema = z.infer<typeof registerSchema>;
