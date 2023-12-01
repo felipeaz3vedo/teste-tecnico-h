1fcd1f8ba71a90ca34c587e17f182eaa42fc31eb
Felipe Pessoa <felipedeazevedopessoa@gmail.com>
2023-11-30 23:57:27 -0300

    chore(backend): enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+  });
+
   await app.listen(process.env.PORT || 5000);
 }
 
